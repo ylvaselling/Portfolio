5aa7f2fcf854cb87d75e9f14781a07f2f56a3298
Ylva Selling <ylva.selling@gmail.com>
2019-06-08 18:57:16 +0200

    First try with json data

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 "use strict";
 
+// Retrieving data:
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var portfolioData = JSON.parse(this.responseText);
+      appendData(portfolioData);
+      console.log(portfolioData);
+    }
+  };
+  xhttp.open("GET", "data.json", true);
+  xhttp.send();
+}
+
 $(function() {
     $('.category').change(toggleVisibility);
 })
+
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'id: ' + data [i].id + ' Name: ' + data[i].firstName + ' ' + data[i].lastName;
+    mainContainer.appendChild(div);
+  }
+}
 
 function showAll() {
 
